9d34ac50fcb88b05c16ca51cb292a8e2f2e335d7
Stacy Howerton <stacyhowerton@Stacys-iMac.lan1>
2018-08-06 05:18:52 -0400

    Reorganized tests into Function Calls to avoid unneccessary repetition there.

--- cypress/integration/wordsmith_practice.js
+++ cypress/integration/wordsmith_practice.js
@@ -1,26 +1,36 @@
 describe('Visit AI', function() {
 
-  it('Everything', function() {
-    //split up into separate actions later.
+  it('Test 1 -- Valid Rule', function() {
+    login();
+    addProject();
+    addBranchWithRule('temperature > 70', 'It\'s hot');
+    cy.get('.branch').should('contain', 'It\'s hot')
+  })
 
-    //--LOGIN--How to handle login info??
-    cy.visit ('')
-    cy.get('.mdl-textfield__input.string.email.optional')
-    //cy.get('input').find('user-email') -- why can't I use id's??
-      .type('')
-    cy.get('.mdl-textfield__input.password.optional')
-      .type('')
-    cy.get(".btn").click()
+  it('Test 2 -- Invalid Rule', function() {
+    login();
+    addProject();
+    addBranchWithRule('temperature > 70\'', 'It\'s hot');
+    cy.get('h2.modal__title')
+      .should('contain', 'This branch is invalid.')
+  })
 
-    //--CREATE NEW PROJECT--
+  function login() {
+    cy.visit ('')//base url in cypress.json
+      .get('.mdl-textfield__input.string.email.optional')
+      .type('')
+      .get('.mdl-textfield__input.password.optional')
+      .type('')
+      .get(".btn").click()
+  }
+
+  function addProject() {
     cy.contains('New Project').click()
     /*cy.contains('Upload CSV File').click()
     cy.get('.mdl-textfield__input.ws-input')
       .type('Awesome Project')
     cy.contains('browse for a file').click()
     //play more with this later*/
-
-    // --Fill in table -- improve later
     cy.get('thead>tr').eq(0).find('.ws-input').eq(0)
       .type('city')
     cy.get('thead>tr').eq(0).find('.ws-input').eq(1)
@@ -45,35 +55,17 @@ describe('Visit AI', function() {
       .type('101')
     cy.get('tbody>tr').eq(2).find('.ws-input').eq(2)
       .type('20')
-
     cy.contains('Create Project').click()
+  }
 
-    //--INSERT BRANCH--
-    cy.contains('Add Branch').click()
-    //--FILL OUT RULE--
-    //--FOR TEST 1--
+  function addBranchWithRule(ruleInput, thenInput) {
+    cy.contains('Add Branch').click({timeout: 20000})
     cy.contains('Write out the first rule that is true.').click()
     cy.get('.public-DraftStyleDefault-block.public-DraftStyleDefault-ltr').eq(0)
-      .type('temperature > 70')
-    cy.get('.public-DraftStyleDefault-block.public-DraftStyleDefault-ltr').eq(1)
-      .type('It\'s hot')
+      .type(ruleInput)
+      .get('.public-DraftStyleDefault-block.public-DraftStyleDefault-ltr').eq(1)
+      .type(thenInput)
     cy.contains('Done').click()
-    //--VALIDATION--
-    //Validate rule inserted correctly
-    //cy.get('.branch').should('eq', 'It\'s hot')
-    //refine this!
-
-    /*//--FOR TEST 2--
-    cy.contains('Write out the first rule that is true.').click()
-    cy.get('.public-DraftStyleDefault-block.public-DraftStyleDefault-ltr').eq(0)
-      .type('temperature > 70\'')
-    cy.get('.public-DraftStyleDefault-block.public-DraftStyleDefault-ltr').eq(1)
-      .type('It\'s hot')
-    cy.contains('Done').click()
-    //--VALIDATION--
-    //Validate error message here
-    //expect('.modal__title').to.eq('This branch is invalid. Continue anyway?')
-    //refine!!*/
-  })
+  }
 
 })
