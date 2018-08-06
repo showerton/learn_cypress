09fb31597df988af8538ade05dfa405c654c0f16
Stacy Howerton <stacyhowerton@Stacys-iMac.lan1>
2018-08-06 06:12:55 -0400

    Refined the tests a bit more.

--- cypress/integration/wordsmith_practice.js
+++ cypress/integration/wordsmith_practice.js
@@ -17,49 +17,39 @@ describe('Visit AI', function() {
 
   function login() {
     cy.visit ('')//base url in cypress.json
-      .get('.mdl-textfield__input.string.email.optional')
-      .type('')
-      .get('.mdl-textfield__input.password.optional')
-      .type('')
+      .get('.mdl-textfield__input.string.email.optional').type('')
+      .get('.mdl-textfield__input.password.optional').type('')
       .get(".btn").click()
   }
 
   function addProject() {
+    var header = ['city', 'temperature', 'chance_of_rain']
+    var row1 = ['Chicago', '84', '0']
+    var row2 = ['Amsterdam', '65', '54']
+    var row3 = ['Dallas', '101', '20']
     cy.contains('New Project').click()
     /*cy.contains('Upload CSV File').click()
-    cy.get('.mdl-textfield__input.ws-input')
-      .type('Awesome Project')
+    cy.get('.mdl-textfield__input.ws-input').type('Awesome Project')
     cy.contains('browse for a file').click()
     //play more with this later*/
-    cy.get('thead>tr').eq(0).find('.ws-input').eq(0)
-      .type('city')
-    cy.get('thead>tr').eq(0).find('.ws-input').eq(1)
-      .type('temperature')
-    cy.get('thead>tr').eq(0).find('.ws-input').eq(2)
-      .type('chance_of_rain')
-    cy.get('tbody>tr').eq(0).find('.ws-input').eq(0)
-      .type('Chicago')
-    cy.get('tbody>tr').eq(0).find('.ws-input').eq(1)
-      .type('84')
-    cy.get('tbody>tr').eq(0).find('.ws-input').eq(2)
-      .type('0')
-    cy.get('tbody>tr').eq(1).find('.ws-input').eq(0)
-      .type('Amsterdam')
-    cy.get('tbody>tr').eq(1).find('.ws-input').eq(1)
-      .type('65')
-    cy.get('tbody>tr').eq(1).find('.ws-input').eq(2)
-      .type('54')
-    cy.get('tbody>tr').eq(2).find('.ws-input').eq(0)
-      .type('Dallas')
-    cy.get('tbody>tr').eq(2).find('.ws-input').eq(1)
-      .type('101')
-    cy.get('tbody>tr').eq(2).find('.ws-input').eq(2)
-      .type('20')
+    //TODO -- put this in a loop of some kind
+    cy.get('thead>tr').eq(0).find('.ws-input').eq(0).type(header[0])
+      .get('thead>tr').eq(0).find('.ws-input').eq(1).type(header[1])
+      .get('thead>tr').eq(0).find('.ws-input').eq(2).type(header[2])
+      .get('tbody>tr').eq(0).find('.ws-input').eq(0).type(row1[0])
+      .get('tbody>tr').eq(0).find('.ws-input').eq(1).type(row1[1])
+      .get('tbody>tr').eq(0).find('.ws-input').eq(2).type(row1[2])
+      .get('tbody>tr').eq(1).find('.ws-input').eq(0).type(row2[0])
+      .get('tbody>tr').eq(1).find('.ws-input').eq(1).type(row2[1])
+      .get('tbody>tr').eq(1).find('.ws-input').eq(2).type(row2[2])
+      .get('tbody>tr').eq(2).find('.ws-input').eq(0).type(row3[0])
+      .get('tbody>tr').eq(2).find('.ws-input').eq(1).type(row3[1])
+      .get('tbody>tr').eq(2).find('.ws-input').eq(2).type(row3[2])
     cy.contains('Create Project').click()
   }
 
   function addBranchWithRule(ruleInput, thenInput) {
-    cy.contains('Add Branch').click({timeout: 20000})
+    cy.contains('Add Branch').click({timeout: 30000})
     cy.contains('Write out the first rule that is true.').click()
     cy.get('.public-DraftStyleDefault-block.public-DraftStyleDefault-ltr').eq(0)
       .type(ruleInput)
